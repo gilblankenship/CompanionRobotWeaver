f9f694845e93fe1fccf02c2ed7a6ca518257a29a
gilblankenship <57296748+gilblankenship@gmail.com>
2020-10-16 05:12:02 -0400

    Initial commit

---
+++ Team2Code/README.md
@@ -0,0 +1,21 @@
+# Group2-Autonomous-Interacting-Robot
+group 2 master
+
+to run first call ./ngrok http 5000
+then update alexa endpoint to new URL
+run python3 weaverv2.py
+
+if you force quit weaver killall python3 processes
+
+Weaver is main + Alexa backed 
+Cv controller does everything needing opencv
+Heartbeatcontorller is bonus fun thing 
+Chat handles robot to robot comms
+April is functions and constants for swarming
+
+@other-groups
+Feel free to take weaver, 
+and look at how to integrate other
+Scripts as processes and threads.
+
+For every get call task_done once or join will fail to sync
---
+++ weaverv2.py
@@ -0,0 +1,162 @@
+import queue as qu
+import threading as th
+import multiprocessing as mp
+import time
+
+from flask import Flask, render_template
+from flask_ask import Ask, statement, question
+
+import openCVController
+import heartbeatController
+import chatController
+import aprilTags
+
+# this file calls all others as threads then moves data from queue
+
+# "terminate" - end the thread please
+# "halt", "personFollow", "aprilFollow", "eyeballFollow"
+# "request"
+
+app = Flask(__name__)
+ask = Ask(app, '/')
+
+cvQueue = mp.JoinableQueue()
+heartbeatQueue = qu.Queue()
+chatQueue = qu.Queue()
+checkChatQueueQueue = qu.Queue()
+
+allQueues = []
+allQueues.append(cvQueue)
+allQueues.append(heartbeatQueue)
+allQueues.append(chatQueue)
+allQueues.append(checkChatQueueQueue)
+
+@app.route('/')
+def hello_world():
+	return "Flask Server is Online!"
+
+@ask.launch
+def launched():
+	return question("hello. what would you like me to do?").reprompt(
+		"if you don't need me, please put me to sleep.")
+
+@ask.intent('AMAZON.FallbackIntent')
+def default():
+	return question("Sorry, I don't understand that command. What would you like me to do?").reprompt(
+		"What would you like me to do now?")
+
+@ask.intent('SleepIntent')
+def sleep():
+	return statement('goodnight')
+
+@ask.intent('terminate')
+def terminate():
+    for queue in allQueues:
+        queue.put("terminate")
+    return statement('processes are terminated')
+
+@ask.intent('testComunication')
+def testComunications():
+	return statement('Jetson Backend is opperational. If it was not	I would not be alive')
+
+@ask.intent('trackMe')
+def trackMe():
+    cvQueue.put("personFollow")
+    cvQueue.join()
+    return statement('I\'m following you now. Please do not run')
+
+@ask.intent('lookAtMe')
+def lookAtMe():
+    cvQueue.put('eyeballFollow')
+    cvQueue.join()
+    return statement('I can see you')
+
+@ask.intent('stopActing')
+def stopActing():
+    cvQueue.put("halt")
+    cvQueue.join()
+    return statement('I\'m done following you')
+
+@ask.intent('callForHelp')
+def callForHelp():
+    disTh = th.Thread(target= distressHandling)
+    disTh.start()
+    return statement('Sending distress signal')
+
+def distressHandling():
+    # ask CV for my coords
+    cvQueue.put("getCoordinates")
+    cvQueue.join()
+    x_cord = cvQueue.get()
+    z_cord = cvQueue.get()
+    cvQueue.task_done()
+    cvQueue.task_done()
+    # pass my coords to the chat server
+    chatQueue.put("sendDistress")
+    chatQueue.put(x_cord)
+    chatQueue.put(z_cord)
+    chatQueue.join()
+    print("finished bonus thread")
+
+@ask.intent('getHeartbeat')
+def getHeartbeat():
+    heartbeatQueue.put("request")
+    while not heartbeatQueue.empty():
+        pass
+    heartrate = heartbeatQueue.get()
+    return statement("Your heartrate is " + str(heartrate) + " beats per minute")
+
+@ask.intent('takePhoto')
+def takePhoto():
+    cvQueue.put('photo')
+    cvQueue.join()
+    return statement("Your photo will be taken and emailed to you")
+
+def checkChatQueue():
+    threadActive = True
+    while threadActive:
+        time.sleep(.1)
+        if not checkChatQueueQueue.empty():
+            if checkChatQueueQueue.get() == "terminate":
+                threadActive = False
+        if not chatQueue.empty():
+            request = chatQueue.get()
+            chatQueue.task_done()
+            if request == "terminate":
+                chatQueue.put(request)
+            elif request == "receivedDistress":
+                print("i got distress sign")
+                x_cord = chatQueue.get()
+                z_cord = chatQueue.get()
+                chatQueue.task_done()
+                chatQueue.task_done()
+                final_target  = aprilTags.getClosestTag(x_cord, z_cord, False)
+                print("target dest: ", final_target[0], " ",final_target[1])
+                cvQueue.put("aprilFollow")
+                cvQueue.put(final_target[0])
+                cvQueue.put(final_target[1])
+                cvQueue.join()
+
+if __name__ == '__main__':
+    alexaTh = th.Thread(target= app.run)
+    alexaTh.daemon = True
+    cvTh = mp.Process(target= openCVController.run, args= (cvQueue, ))
+    heartbeatTh = th.Thread(target= heartbeatController.run, args= (heartbeatQueue, ))
+    chatTh = th.Thread(target= chatController.run, args= (chatQueue, ))
+    chatQueueCheckTh = th.Thread(target= checkChatQueue)
+
+    allThreads = []
+    allThreads.append(cvTh)
+    allThreads.append(heartbeatTh)
+    allThreads.append(chatTh)
+    allThreads.append(chatQueueCheckTh)
+
+    alexaTh.start()
+
+    for thread in allThreads:
+        thread.start()
+
+    for thread in allThreads:
+        thread.join()
+    
+    print("exiting")
