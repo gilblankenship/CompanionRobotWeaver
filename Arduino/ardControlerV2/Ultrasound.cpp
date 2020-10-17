568a9fda236bd08fbbf39c1d588b0b9e315e7d76
gilblankenship <57296748+gilblankenship@gmail.com>
2020-10-17 04:45:38 -0400

    Moved Arduino files to the same directory Modified the xTaskCreate commands to remove portChar declaration compile error. 2020-10-17: Some minor edits in Arduino code.

--- Arduino/ardControlerV2/Ultrasound.cpp
+++ Arduino/ardControlerV2/Ultrasound.cpp
@@ -45,7 +45,7 @@ long Ultrasound::getDistance()
 long Ultrasound::microsecondsToInches(long microseconds) {
   // According to Parallax's datasheet for the PING))), there are 73.746
   // microseconds per inch (i.e. sound travels at 1130 feet per second).
-  // This gives the distance travelled by the ping, outbound and return,
+  // This gives the distance travelled by the ping pulse, outbound and return,
   // so we divide by 2 to get the distance of the obstacle.
   // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
   return microseconds / 74 / 2;
