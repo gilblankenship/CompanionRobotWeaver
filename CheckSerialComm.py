0537f8b274910b77f38d13cd2977fb21b1ef98d4
gilblankenship <57296748+gilblankenship@gmail.com>
2020-10-26 12:29:30 -0400

    Added serial communications example

--- CheckSerialComm.py
+++ CheckSerialComm.py
@@ -1,14 +1,10 @@
+# Simple program to read data from the Arduino serial port
 import serial
-ser = serial.Serial("COM4",9600)
+ser = serial.Serial("COM4",9600) # Set to the appropriate port
+# ser = serial.Serial("/dev/ttyUSB0",9600) # Linux port, check this
 ser.flushInput()
 k=0
-while k<5:
+while k<10:
     linein = ser.readline()
-    print(k,str(linein))
-    # if len(str(linein))==7:
-    #     print(str(k), ' ',len(str(linein)),' ',str(linein)[2:4])
-    # else:
-    #     print(str(k), ' ',len(str(linein)),' ',str(linein)[2:3])
-    # print(str(linein)[2:4])
-    # print(len(str(linein)),' ',str(linein))
+    print(k,',',str(linein))
     k=k+1
