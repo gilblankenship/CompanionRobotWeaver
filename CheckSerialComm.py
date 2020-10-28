0f3457f0275db8ac653e3b46da99ae9dd28e5d78
gilblankenship <57296748+gilblankenship@gmail.com>
2020-10-28 06:55:58 -0400

    Updated code

--- CheckSerialComm.py
+++ CheckSerialComm.py
@@ -1,35 +1,32 @@
 # Simple program to read data from the Arduino serial port
-import struct
+import numpy as np
 import serial
-# import platform
-#
-# plat = platform.platform().lower()
-#
-# if plat.find('win') != -1:
-#     print("Windows OS - setting port to COM4 - check this")
-#     ser = serial.Serial("COM4", 9600)  # Set to the appropriate port
-# elif plat.find('lin') != -1:
-#     print("Linux OS - setting port to /dev/ttyUSB0 - check this")
-#     ser = serial.Serial("/dev/ttyUSB0", 9600)  # Linux port, check this
-# else:  # Assume Mac OS
-#     print("Assuming MAC OS - setting port to /dev/ttyUSB0 - check this")
-#     ser = serial.Serial("/dev/ttyUSB0", 9600)  # Linux port, check this
+import matplotlib.pylab as plt
+import platform
+
+plat = platform.platform().lower()
+if plat.find('win') != -1:
+    print("Windows OS - setting port to COM4 - check this")
+    ser = serial.Serial("COM4", 9600)  # Set to the appropriate port
+elif plat.find('lin') != -1:
+    print("Linux OS - setting port to /dev/ttyUSB0 - check this")
+    ser = serial.Serial("/dev/ttyUSB0", 9600)  # Linux port, check this
+else:  # Assume Mac OS
+    print("Assuming MAC OS - setting port to /dev/ttyUSB0 - check this")
+    ser = serial.Serial("/dev/ttyUSB0", 9600)  # Linux port, check this
+ser.flushInput()
 
 # import sys
 # sys.exit(0)
-ser = serial.Serial("COM4", 9600)  # Set to the appropriate port
 
-ser.flushInput()
 k = 0
-while k < 10:
+numValues=10
+x = np.empty(shape=numValues, dtype=float)  # 3 element array
+for k in range(numValues):
     linein = ser.readline()
-    # print(str(linein))
-    start = 2
-    x = struct.unpack('f', linein)
-    if x < 0:
-        end = 9
-    else:
-        end = 10
-    rang = [*range(start, end)];
-    print(k, ',', str(x)[rang])
-    k = k + 1
+    xs=str(linein)
+    xf=float(xs[2:-3])
+    x[k]=xf
+plt.plot(x)
+plt.grid()
+plt.show()
