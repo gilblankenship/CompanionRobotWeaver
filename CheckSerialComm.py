ab842a7488444085e5844a101ac1969d64bc8391
gilblankenship <57296748+gilblankenship@gmail.com>
2020-10-28 09:59:13 -0400

    Updated code

--- CheckSerialComm.py
+++ CheckSerialComm.py
@@ -6,8 +6,9 @@ from PlotData import PlotData
 
 plat = platform.platform().lower()
 if plat.find('win') != -1:
-    print("Windows OS - setting port to COM4 - check this")
-    ser = serial.Serial("COM4", 9600)  # Set to the appropriate port
+    print("Windows OS - setting port to COM4 or COM6 - check this")
+    ser = serial.Serial("COM4", 9600)  # Port for Arduino NANO
+    # ser = serial.Serial("COM6", 9600)  # Port for Arduino NANO BLE
 elif plat.find('lin') != -1:
     print("Linux OS - setting port to /dev/ttyUSB0 - check this")
     ser = serial.Serial("/dev/ttyUSB0", 9600)  # Linux port, check this
@@ -17,11 +18,13 @@ else:  # Assume Mac OS
 ser.flushInput()
 # import sys
 # sys.exit(0)
-k = 0
 numValues=10
-x = np.empty(shape=numValues, dtype=float)  # 3 element array
+x = np.empty(shape=numValues, dtype=float)  # initialize empty array
+print("Start loop")
 for k in range(numValues):
+    print(k)
     linein = ser.readline()
+    print(linein)
     xs=str(linein)
     xf=float(xs[2:-3])
     x[k]=xf
